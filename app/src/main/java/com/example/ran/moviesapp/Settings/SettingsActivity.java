9581ffdc8e649b121f69fda7ec4d74e049af0fb3
Fiadh <fiadh.mohanna@gmail.com>
2019-04-01 21:13:07 +0300

    Complete TODOs

--- app/src/main/java/com/example/ran/moviesapp/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/ran/moviesapp/Settings/SettingsActivity.java
@@ -14,61 +14,120 @@ import com.example.ran.moviesapp.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.settings_activity);
+
     }
 
+// sharedePreference file android (value) //
+
+    // Preference
+
     public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
         @Override
+
         public void onCreate(Bundle savedInstanceState) {
+
             super.onCreate(savedInstanceState);
 
+
+
             //First link this fragment with the PreferenceScreen
+
             addPreferencesFromResource(R.xml.settings);
 
+
+
             //Define the  Preference, set its summary, and set onChangeListener
+
             Preference sortByPreference = findPreference(getString(R.string.sort_by_key));
+
             setUpPreference(sortByPreference);
+
         }
+
+
 
         @Override
+
         public boolean onPreferenceChange(Preference preference, Object newValue) {
 
+
+
             ListPreference listPreference = (ListPreference) preference;
+
             String newSortBy = newValue.toString();
 
+
+
             //get the index of the selected value in the values array
+
             int index = listPreference.findIndexOfValue(newSortBy);
 
+
+
             //get the entry associated with the new value that was selected and set that as a summary
+
             if (index >= 0) {
+
                 CharSequence[] entries = listPreference.getEntries();
+
                 preference.setSummary(entries[index]);
+
             }
+
             return true;
+
         }
+
+
+
 
 
         private void setUpPreference(Preference preference) {
+
             /*Get the value saved in SharedPreferences for this preference
+
             Then use that value to pass it to the onPreferenceChangeListener of the preference
+
             inside onPreference we'll set the summary since every time the preference changes the summary should change*/
 
-            //TODO 1: Get the SharedPreferences of the preference received in the method parameters
-          
 
-            //TODO 2: Get the selected value corresponding to that preference from SahredPreferences and call it "selectedSortValue"
-            
+
+            //TODO 1: Get the SharedPreferences of the preference received in the method parameters
+
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+
+            //TODO 2: Get the selected value corresponding to that preference from SahredPreferences
+
+            String selectedSortValue = sharedPreferences.getString(preference.getKey(), getString(R.string.best_movies_pref));
+
+
 
             //set the onChangeListener for this preference
+
             preference.setOnPreferenceChangeListener(this);
 
+
+
             //call onPreferenceChange callback so we can set the initial summary of the Preference (When we first open the app)
+
             onPreferenceChange(preference, selectedSortValue);
+
         }
 
+
+
     }
+
+
 
 }
--- app/src/main/java/com/example/ran/moviesapp/UI/MainActivity.java
+++ app/src/main/java/com/example/ran/moviesapp/UI/MainActivity.java
@@ -59,7 +59,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     // TODO 14: Override onRestart to restart the Loader in it, so that when the user is back from the settings Activity, the data can be reloaded accordingly
-    
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        getLoaderManager().restartLoader(LOADER_ID, null, this);
+
+    }
 
     @Override
     public Loader<ArrayList<MovieModel>> onCreateLoader(int i, Bundle bundle) {
@@ -69,55 +76,91 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoadFinished(Loader<ArrayList<MovieModel>> loader, ArrayList<MovieModel> movieModels) {
         //TODO 7: Clear any old data in the movies ArrayList (so in case of a new reload, your app will display only the new data without the old one)
-        
+        adapter.movies.clear();
         //TODO 8: Add moviesModels ArrayList (returned by the Loader after executing the network request) to movies ArrayList (which is associated with the adapter)
-        
+        adapter.movies.addAll(movieModels);
         //TODO 9: Notify the adapter that the ArrayList it's associated with (movies) was changed (hint: use notifyDataSetChanged)
-        
+        adapter.notifyDataSetChanged();
     }
 
     @Override
     public void onLoaderReset(Loader<ArrayList<MovieModel>> loader) {
         //TODO 10: Clear any old data in the ArrayList associated with the adapter
-        
+        adapter.movies.clear();
         //TODO 11: Notify the adapter that the ArrayList it's associated with (movies) was changed
-        
+        adapter.notifyDataSetChanged();
     }
 
     private String buildURL() {
 
+
+
         /*Note: As per the documentation here: https://www.themoviedb.org/documentation/api/discover
+
         To get the best movies you should use: http://api.themoviedb.org/3/discover/movie?primary_release_year=the_year&sort_by=vote_average.desc&api_key=your_key
+
         To get the best drama movies you should use: http://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2018&api_key=f605efa3d15575c1e01480333cb8a356
+
         */
 
-        //local variables related to the URL:
-        final String BASE_URL = "http://api.themoviedb.org/3/discover/movie";
-        final String API_KEY_QUERY_PARAMETER = "api_key";
-        //TODO 3: Insert the value of your API key below
-        final String API_KEY = "";
 
-        //TODO 4: Get an object of SharedPreferences then get the selected sort value from SharedPreferences and call it "sortByValue"
-        
-        
+
+        //local variables related to the URL:
+
+        final String BASE_URL = "http://api.themoviedb.org/3/discover/movie";
+
+        final String API_KEY_QUERY_PARAMETER = "api_key";
+
+        final String API_KEY = "f605efa3d15575c1e01480333cb8a356";
+
+
+
+        //TODO 3: Get the selected sort value from SharedPreferences
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String sortByValue = sharedPrefs.getString(getString(R.string.sort_by_key), getString(R.string.best_movies_pref));
+
+
 
         //Build the URI according to the selected value
+
         Uri uri = Uri.parse(BASE_URL);
+
         Uri.Builder builder = uri.buildUpon();
+
         builder.appendQueryParameter("primary_release_year", "2018");
 
-        /*Check if sortByValue has value equal to best_movies_pref in the Strings file
+
+
+        /*TODO 4: Check if sortByValue has value equal to best_movies_pref in the Strings file
+
         /(because best_movies_pref is the value associated with the Preference that represents the selection of displaying the best movies in the settings screen)*/
+
         if (sortByValue.equals(getString(R.string.best_movies_pref))){
+
             builder.appendQueryParameter("sort_by", "vote_average.desc");
+
         } else {
+
+            builder.appendQueryParameter("with_genres", "18");
+
             //TODO 5: Append a query parameter for the genres (because in this case the user prefers to get best drama movies) and set it's value to 18 (18 is for Drama).
-            
+
         }
 
+
+
         //TODO 6: Append a query parameter for your API key
-        
+
+        builder.appendQueryParameter(API_KEY_QUERY_PARAMETER, API_KEY);
+
+
+
+
+
         return builder.toString();
+
     }
 
     private Boolean isConnected() {
